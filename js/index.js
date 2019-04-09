f56c033871fb955a32ecdcf213dccea9d52592fe
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-09 16:11:44 -0700

    update js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+
+var buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(function(){
+	addEventListener("mouseover", function(event) {   
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+});
